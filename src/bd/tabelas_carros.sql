5e5d0247b83112dfaa33335e3d3dc9a35da44b62
Jamile Cruz <130008726+milecruz26@users.noreply.github.com>
2024-05-21 15:58:05 -0300

    :tada: Primeiro Commit

---
+++ src/bd/tabelas_carros.sql
@@ -0,0 +1,15 @@
+create table carros(
+  id serial primary key,
+  modelo text,
+  marca text,
+  descricao text,
+  valor integer,
+  categoria text
+);
+
+create table alugados(
+  id serial primary key,  
+  carro_id integer references carros(id),
+  retirada timestamp,
+  devolucao timestamp
+);
