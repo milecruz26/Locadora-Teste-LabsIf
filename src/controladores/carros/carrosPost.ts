545699e55bd199c4d1dbabd1724c37a82c80736b
Jamile Cruz <130008726+milecruz26@users.noreply.github.com>
2024-05-22 18:56:40 -0300

    feat: feito a rota post de carro

--- src/controladores/carros/carrosPost.ts
+++ src/controladores/carros/carrosPost.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from "express";
+import { knex } from "../../bd/conexao";
+import { Carro } from "../../utils/tipos";
 
 // [POST] /carros :Criar um carro, deve permitir que um usuário crie um novo carro, fornecendo as informações básicas, como modelo, marca, descrição, preço de aluguel e categoria.
-const cadastrarCarro = async (req: Request, res: Response) => {};
+const cadastrarCarro = async (req: Request, res: Response) => {
+  const { modelo, marca, descricao, valor, categoria, disponivel } = req.body;
+  try {
+    const carro = await knex<Omit<Carro, "id">>("carros")
+      .insert({ modelo, marca, descricao, valor, categoria, disponivel })
+      .returning("*");
+    return res.status(201).json(carro[0]);
+  } catch {
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+};
 
 // [POST] /carros/alugar : Alugar um carro, deve permitir que um usuário alugue um carro, fornecendo o ID do carro, a data e hora de início do aluguel e a data e hora de fim do aluguel.
 const alugarCarro = async (req: Request, res: Response) => {};
